458e8ba926a192133607e87452d0ab25503f0fae
Rafael de Luca <deluca1712@gmail.com>
2023-06-22 17:01:14 -0300

    Mocking data and calculate invoice in 2 different situations

--- src/solutionWithoutInterface/application/ProgramTest.java
+++ src/solutionWithoutInterface/application/ProgramTest.java
@@ -17,28 +17,38 @@ public class ProgramTest {
         Locale.setDefault(Locale.US);
         DateTimeFormatter  dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 
+        /*
         RSTaxService rsTaxService = new RSTaxService();
         System.out.println(rsTaxService.taxValue(100)); // 20
         System.out.println(rsTaxService.taxValue(200)); // 30
+        */
+
+        // mockData less than 12 hours
+        LocalDateTime beginDate = LocalDateTime.parse("17/12/2021 10:30",dateTimeFormatter);
+        LocalDateTime endDate = LocalDateTime.parse("17/12/2021 14:40",dateTimeFormatter);
+        Book mmm = new Book("The Mythical Man-Month");
+        BookRental bookRental = new BookRental(beginDate,endDate,mmm);
 
         double pricePerHour = 10.0;
         double pricePerDay = 130.0;
-        double basicPayment = 50.0;
-        double tax = 10.0;
-        LocalDateTime beginDate = LocalDateTime.parse("17/12/2021 10:30",dateTimeFormatter);
-        LocalDateTime endDate = LocalDateTime.parse("17/12/2021 14:40",dateTimeFormatter);
-        Book elite = new Book("A elite do Atraso");
-        Invoice invoice = new Invoice(basicPayment,tax);
+        RentalBookService rentalBookService = new RentalBookService(pricePerDay, pricePerHour, new RSTaxService());
+        // this function create the invoice base on the given data
+        rentalBookService.createInvoice(bookRental);
 
-        BookRental bookRental = new BookRental(beginDate,endDate,elite);
-        bookRental.setInvoice(invoice);
-        RentalBookService rentalBookService = new RentalBookService(pricePerHour, pricePerDay, new RSTaxService());
-
-
-        System.out.println("\nInvoice Data");
+        System.out.println("\nInvoice Data 01");
         System.out.println("Basic payment: " + bookRental.getInvoice().getBasicPayment());
         System.out.println("Tax: " + bookRental.getInvoice().getTax());
         System.out.println("Total payment: " + bookRental.getInvoice().totalPayment());
 
+        // mock data, rent more than 12 hours
+        beginDate = LocalDateTime.parse("20/12/2021 10:30",dateTimeFormatter);
+        endDate = LocalDateTime.parse("22/12/2021 11:40",dateTimeFormatter);
+        BookRental bookRentalMoreThan12Hours = new BookRental(beginDate,endDate, mmm);
+        rentalBookService.createInvoice(bookRentalMoreThan12Hours);
+
+        System.out.println("\nInvoice Data 02");
+        System.out.println("Basic payment: " + bookRentalMoreThan12Hours.getInvoice().getBasicPayment());
+        System.out.println("Tax: " + bookRentalMoreThan12Hours.getInvoice().getTax());
+        System.out.println("Total payment: " + bookRentalMoreThan12Hours.getInvoice().totalPayment());
     }
 }
--- src/solutionWithoutInterface/services/RentalBookService.java
+++ src/solutionWithoutInterface/services/RentalBookService.java
@@ -3,6 +3,8 @@ package solutionWithoutInterface.services;
 import solutionWithoutInterface.entities.BookRental;
 import solutionWithoutInterface.entities.Invoice;
 
+import java.time.Duration;
+
 public class RentalBookService {
 
     private Double pricePerDay;
@@ -17,7 +19,23 @@ public class RentalBookService {
 
     public void createInvoice(BookRental bookRental) {
 
-        bookRental.setInvoice(new Invoice());
+        double minutes = Duration.between(bookRental.getBeginDate(), bookRental.getEndDate()).toMinutes();
+        double fractionHours = minutes /60.0;
+
+        double basicPayment = 0.0;
+        // payment calculate in days if hours bigger than 12 hours,
+        // payment calculete in hours if hours less than 12 hours
+        int limiteHours = 12;
+        if (fractionHours > limiteHours) {
+            int days = (int) Math.ceil(fractionHours / 24);
+            basicPayment = this.pricePerDay * days;
+        } else {
+            basicPayment = this.pricePerHour * Math.ceil(fractionHours);
+        }
+
+        double tax = rsTaxService.taxValue(basicPayment);
+
+        bookRental.setInvoice(new Invoice(basicPayment,tax));
     }
 
 
