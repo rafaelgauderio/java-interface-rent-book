52ddb486238b784c6912ec9f9746c27e901021e5
Rafael de Luca <deluca1712@gmail.com>
2023-06-22 11:04:49 -0300

    entities

---
+++ src/solutionWithoutInterface/entities/BookRental.java
@@ -0,0 +1,53 @@
+package solutionWithoutInterface.entities;
+
+import java.time.LocalDateTime;
+
+public class BookRental {
+
+    private LocalDateTime beginDate;
+    private LocalDateTime endDate;
+    private Book book;
+    private Invoice invoice;
+
+    public BookRental () {
+
+    }
+    // in the begin of the rental we do not have a invoice yet
+    public BookRental(LocalDateTime beginDate, LocalDateTime endDate, Book book) {
+        this.beginDate = beginDate;
+        this.endDate = endDate;
+        this.book = book;
+    }
+
+    public LocalDateTime getBeginDate() {
+        return beginDate;
+    }
+
+    public void setBeginDate(LocalDateTime beginDate) {
+        this.beginDate = beginDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public Invoice getInvoice() {
+        return invoice;
+    }
+
+    public void setInvoice(Invoice invoice) {
+        this.invoice = invoice;
+    }
+}
