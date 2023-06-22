1bd54c5df53bc1c089355235d1ed36278d39cf53
Rafael de Luca <deluca1712@gmail.com>
2023-06-22 17:06:02 -0300

    Using function to format and printing invoice

--- src/solutionWithoutInterface/application/ProgramTest.java
+++ src/solutionWithoutInterface/application/ProgramTest.java
@@ -15,7 +15,7 @@ public class ProgramTest {
     public static void main(String[] args) {
 
         Locale.setDefault(Locale.US);
-        DateTimeFormatter  dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 
         /*
         RSTaxService rsTaxService = new RSTaxService();
@@ -24,31 +24,31 @@ public class ProgramTest {
         */
 
         // mockData less than 12 hours
-        LocalDateTime beginDate = LocalDateTime.parse("17/12/2021 10:30",dateTimeFormatter);
-        LocalDateTime endDate = LocalDateTime.parse("17/12/2021 14:40",dateTimeFormatter);
+        LocalDateTime beginDate = LocalDateTime.parse("17/12/2021 10:30", dateTimeFormatter);
+        LocalDateTime endDate = LocalDateTime.parse("17/12/2021 14:40", dateTimeFormatter);
         Book mmm = new Book("The Mythical Man-Month");
-        BookRental bookRental = new BookRental(beginDate,endDate,mmm);
+        BookRental bookRental = new BookRental(beginDate, endDate, mmm);
 
         double pricePerHour = 10.0;
         double pricePerDay = 130.0;
         RentalBookService rentalBookService = new RentalBookService(pricePerDay, pricePerHour, new RSTaxService());
         // this function create the invoice base on the given data
         rentalBookService.createInvoice(bookRental);
-
-        System.out.println("\nInvoice Data 01");
-        System.out.println("Basic payment: " + bookRental.getInvoice().getBasicPayment());
-        System.out.println("Tax: " + bookRental.getInvoice().getTax());
-        System.out.println("Total payment: " + bookRental.getInvoice().totalPayment());
+        printinInvoice(bookRental);
 
         // mock data, rent more than 12 hours
-        beginDate = LocalDateTime.parse("20/12/2021 10:30",dateTimeFormatter);
-        endDate = LocalDateTime.parse("22/12/2021 11:40",dateTimeFormatter);
-        BookRental bookRentalMoreThan12Hours = new BookRental(beginDate,endDate, mmm);
+        beginDate = LocalDateTime.parse("20/12/2021 10:30", dateTimeFormatter);
+        endDate = LocalDateTime.parse("22/12/2021 11:40", dateTimeFormatter);
+        BookRental bookRentalMoreThan12Hours = new BookRental(beginDate, endDate, mmm);
         rentalBookService.createInvoice(bookRentalMoreThan12Hours);
+        printinInvoice(bookRentalMoreThan12Hours);
 
-        System.out.println("\nInvoice Data 02");
-        System.out.println("Basic payment: " + bookRentalMoreThan12Hours.getInvoice().getBasicPayment());
-        System.out.println("Tax: " + bookRentalMoreThan12Hours.getInvoice().getTax());
-        System.out.println("Total payment: " + bookRentalMoreThan12Hours.getInvoice().totalPayment());
+    }
+
+    public static void printinInvoice(BookRental bookRental) {
+        System.out.println("\nInvoice Data");
+        System.out.println("Basic payment: " + String.format("%.2f", bookRental.getInvoice().getBasicPayment()));
+        System.out.println("Tax: " + String.format("%.2f", bookRental.getInvoice().getTax()));
+        System.out.println("Total payment: " + String.format("%.2f", bookRental.getInvoice().totalPayment()));
     }
 }
