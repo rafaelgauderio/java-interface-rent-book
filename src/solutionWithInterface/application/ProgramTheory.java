a0d75480bc63952b9a4dc046a1f8a3ed8f6509d2
Rafael de Luca <deluca1712@gmail.com>
2023-06-23 11:59:36 -0300

    Inversion of control and dependency injection

--- src/solutionWithInterface/application/ProgramTheory.java
+++ src/solutionWithInterface/application/ProgramTheory.java
@@ -9,5 +9,14 @@ public class ProgramTheory {
                 "\tInterface allows to create loosely coupled and flexible software systems.\n" +
                 "\tInterface is a public and abstract Class that has only abstract methods with no body.\n" +
                 "\tInterface has only constantes. All variables are final and static.\n");
+
+        System.out.println("\nInversion of controle\n" +
+                "\tIs is a software development pattern that concists of removing the responsability of instantiating\n" +
+                "\t\tits dependencies from the Class.");
+
+        System.out.println("\nDependency injection\n" +
+                "\tIs is one of the ways to carry out the inversion of control.\n" +
+                "\tAn external component instantiates the dependency which is injected by the superclass object.\n" +
+                "\tCan be implemented through many ways, like: constructors, instantiation class, container or frameword.\n");
     }
 }
